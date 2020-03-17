3ea95fba49e2596c49fba8f9bd6ea86c8e028ce3
Anders Mattson <herr.mattson@gmail.com>
2020-03-17 19:02:37 +0100

    Updates

---
+++ subtitles.js
@@ -0,0 +1,9 @@
+var subTitles = [
+    "Bask in the glory of medias uppfinningsrikedom",
+    "Alla ord utom öl.",
+    "Sammansatta smittoord.",
+    "Svenska Akademien ska ju också ha något att göra.",
+    "Sprider sig snabbt i mediavärlden.",
+    "Om man inbillar sig att det handlar om ett brädspel så blir det mycket roligare."
+//    ,"En kurerad lista med mediernas mest kreativa sammansättningar."
+];
